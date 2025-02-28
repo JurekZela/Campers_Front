c980578de9d3d9df0042f7ffe8dd3193d3d2d296
Juri Zelabowski <juri.zelabowski@gmail.com>
2025-02-28 20:44:06 +0100

    add HeaderMenu

--- src/components/Sidebar/Sidebar-styled.js
+++ src/components/Sidebar/Sidebar-styled.js
@@ -2,5 +2,11 @@ import { styled } from 'styled-components';
 
 export const Block = styled.div`
     display: flex;
-    background-color: #F2F4F7;
+    align-items: center;
+    background-color: #F7F7F7;
+    height: 72px;
+    width: 1440px;
+    border: 1px solid #F2F4F7;
 `;
+
+export const HeaderMenu = styled.div``
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,9 +1,14 @@
-import { Block } from './Sidebar-styled.js';
+import { Block, HeaderMenu } from './Sidebar-styled.js';
+import { Link } from 'react-router-dom';
 
 export default function Sidebar() {
   return (
     <Block>
-      Sidebar
+      <img src="../../../public/images/TravelTrucks.svg" width="136px" height="15px" alt="sidebar logo" loading='lazy' />
+      <HeaderMenu>
+        <Link to="/">Home</Link>
+        <Link to="/catalog">Catalog</Link>
+      </HeaderMenu>
     </Block>
   )
 }
