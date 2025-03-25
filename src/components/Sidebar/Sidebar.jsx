77d17f9fa7b9cda68c2d5cba586a2425cf5f4ceb
Juri Zelabowski <juri.zelabowski@gmail.com>
2025-03-25 18:22:57 +0100

    add function changeStatePage

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,12 +1,19 @@
 import { SidebarContainer, HeaderMenu, HeaderHome, HeaderCatalog } from './Sidebar-styled.js';
+import { useDispatch } from 'react-redux';
 
 export default function Sidebar() {
+  const dispatch = useDispatch();
+
+  const changeInitialStatePage = () => dispatch(({
+    page: 1,
+  }))
+
   return (
     <SidebarContainer>
       <img src="/images/TravelTrucks.svg" width="136px" height="15px" alt="sidebar logo" loading='lazy' />
       <HeaderMenu>
         <HeaderHome to="/">Home</HeaderHome>
-        <HeaderCatalog to="/campers">Catalog</HeaderCatalog>
+        <HeaderCatalog onClick={changeInitialStatePage} to="/campers">Catalog</HeaderCatalog>
       </HeaderMenu>
     </SidebarContainer>
   )
