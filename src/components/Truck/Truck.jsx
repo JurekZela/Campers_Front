a30f996264dc996475ed684143ffb4e964da04a5
Juri Zelabowski <juri.zelabowski@gmail.com>
2025-03-26 20:02:56 +0100

    change render truck container

--- src/components/Truck/Truck.jsx
+++ src/components/Truck/Truck.jsx
@@ -35,73 +35,73 @@ export default function Truck() {
   );
 
   return (
-    <TruckContainer>
-      {items.map((item) => (
-        <TruckCard key={item.id}>
-          {item.gallery?.[0]?.thumb && (
-            <TruckImg src={item.gallery[0].thumb} alt={item.name} />
-          )}
-          <ListCardInfo>
-            <InfoPriNam>
-              <TruckName>{item.name}</TruckName>
-              <HeardPrc>
-                <TruckPrice>€{item.price}.00</TruckPrice>
-                <img
-                  src="/icons/heart.svg"
-                  alt="heart"
-                  width="26px"
-                  height="24px"
-                />
-              </HeardPrc>
-            </InfoPriNam>
-
-            <InfoLocMap>
-              {item.rating !== undefined && (
-                <Rating>
-                  <img
-                    src="/icons/Rating.svg"
-                    alt="rating"
-                    width="16px"
-                    height="16px"
-                  />
-                  {item.rating} ({item.reviews?.length || 0} Reviews)
-                </Rating>
+    items &&( <TruckContainer>
+          {items.map((item) => (
+            <TruckCard key={item.id}>
+              {item.gallery?.[0]?.thumb && (
+                <TruckImg src={item.gallery[0].thumb} alt={item.name} />
               )}
-              {item.location && (
-                <Location>
-                  <img
-                    src="/icons/map.svg"
-                    alt="location"
-                    width="16px"
-                    height="16px"
-                  />
-                  {item.location}
-                </Location>
-              )}
-            </InfoLocMap>
+              <ListCardInfo>
+                <InfoPriNam>
+                  <TruckName>{item.name}</TruckName>
+                  <HeardPrc>
+                    <TruckPrice>€{item.price}.00</TruckPrice>
+                    <img
+                      src="/icons/heart.svg"
+                      alt="heart"
+                      width="26px"
+                      height="24px"
+                    />
+                  </HeardPrc>
+                </InfoPriNam>
 
-            <Description>{item.description}</Description>
+                <InfoLocMap>
+                  {item.rating !== undefined && (
+                    <Rating>
+                      <img
+                        src="/icons/Rating.svg"
+                        alt="rating"
+                        width="16px"
+                        height="16px"
+                      />
+                      {item.rating} ({item.reviews?.length || 0} Reviews)
+                    </Rating>
+                  )}
+                  {item.location && (
+                    <Location>
+                      <img
+                        src="/icons/map.svg"
+                        alt="location"
+                        width="16px"
+                        height="16px"
+                      />
+                      {item.location}
+                    </Location>
+                  )}
+                </InfoLocMap>
 
-            <Categories>
-              {item.transmission && renderCategory("/icons/transmission.svg", item.transmission)}
-              {item.engine && renderCategory("/icons/engine.svg", item.engine)}
-              {item.kitchen && renderCategory("/icons/kitchen.svg", "Kitchen")}
-              {item.AC && renderCategory("/icons/ac.svg", "AC")}
-              {item.TV && renderCategory("/icons/tv.svg", "TV")}
-              {item.radio && renderCategory("/icons/ui-radios.svg", "Radio")}
-              {item.bathroom && renderCategory("/icons/ph_shower.svg", "Bathroom")}
-              {item.refrigerator && renderCategory("/icons/refrigerator.svg", "Refrigerator")}
-              {item.microwave && renderCategory("/icons/microwave.svg", "Microwave")}
-              {item.gas && renderCategory("/icons/gas.svg", "Gas")}
-              {item.water && renderCategory("/icons/water.svg", "Water")}
-            </Categories>
+                <Description>{item.description}</Description>
 
-            <ShowMore to={`/campers/${item.id}`} onClick={() => handleShowMore({id: item.id})}>
-              Show More
-            </ShowMore>
-          </ListCardInfo>
-        </TruckCard>
-      ))}
-    </TruckContainer>
+                <Categories>
+                  {item.transmission && renderCategory("/icons/transmission.svg", item.transmission)}
+                  {item.engine && renderCategory("/icons/engine.svg", item.engine)}
+                  {item.kitchen && renderCategory("/icons/kitchen.svg", "Kitchen")}
+                  {item.AC && renderCategory("/icons/ac.svg", "AC")}
+                  {item.TV && renderCategory("/icons/tv.svg", "TV")}
+                  {item.radio && renderCategory("/icons/ui-radios.svg", "Radio")}
+                  {item.bathroom && renderCategory("/icons/ph_shower.svg", "Bathroom")}
+                  {item.refrigerator && renderCategory("/icons/refrigerator.svg", "Refrigerator")}
+                  {item.microwave && renderCategory("/icons/microwave.svg", "Microwave")}
+                  {item.gas && renderCategory("/icons/gas.svg", "Gas")}
+                  {item.water && renderCategory("/icons/water.svg", "Water")}
+                </Categories>
+
+                <ShowMore to={`/campers/${item.id}`} onClick={() => handleShowMore({id: item.id})}>
+                  Show More
+                </ShowMore>
+              </ListCardInfo>
+            </TruckCard>
+          ))}
+        </TruckContainer>)
   );
 }
