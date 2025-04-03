0a09f5e3fa988b9f494d56a3e8507fac4371b665
Juri Zelabowski <juri.zelabowski@gmail.com>
2025-04-03 18:58:20 +0200

    review styled

--- src/components/Reviews/Reviews-styled.js
+++ src/components/Reviews/Reviews-styled.js
@@ -1,5 +1,13 @@
 import {styled} from "styled-components";
 
+export const ReviewsList = styled.ul`
+    display: flex;
+    flex-direction: column;
+    gap: 50px;
+    max-width: 631px;
+    margin: 55px 45px 0 0;
+`;
+
 export const ReviewName = styled.p`
     color: #101828;
     font-weight: 500;
@@ -7,7 +15,13 @@ export const ReviewName = styled.p`
 `;
 
 export const ReviewComment = styled.p`
+    margin-top: 23px;
     color: #475467;
     font-weight: 400;
     font-size: 16px;
+`;
+
+export const UserInfoReview = styled.div`
+    display: flex;
+    gap: 17px;
 `;
--- src/components/Reviews/ReviewsPage.jsx
+++ src/components/Reviews/ReviewsPage.jsx
@@ -4,25 +4,29 @@ import Rating from '@mui/material/Rating';
 import Stack from '@mui/material/Stack';
 import Avatar from '@mui/material/Avatar';
 import { selectTruckById } from '../../redux/catalog/TruckDetailsById/selectors.js';
-import {ReviewList, ReviewName, ReviewComment} from './Reviews-styled.js';
+import { UserInfoReview, ReviewsList, ReviewName, ReviewComment} from './Reviews-styled.js';
 
 export default function ReviewsPage () {
   const camperDetailsInfo = useSelector(selectTruckById);
   const reviews = camperDetailsInfo.reviews;
   return(
-    <ul>
+    <ReviewsList>
       {reviews.map((review) => {
         return (
-          <ReviewList key={nanoid()}>
-            <Avatar sx={{ bgcolor: '#F2F4F7', color: '#E44848' }}>{review.reviewer_name[0]}</Avatar>
-            <ReviewName>{review.reviewer_name}</ReviewName>
-            <Stack spacing={1}>
-            <Rating  name="size-small" defaultValue={review.reviewer_rating} precision={0.5} size="small"   readOnly />
-            </Stack>
+          <li key={nanoid()}>
+            <UserInfoReview>
+              <Avatar sx={{ bgcolor: '#F2F4F7', color: '#E44848' }}>{review.reviewer_name[0]}</Avatar>
+              <div>
+                <ReviewName>{review.reviewer_name}</ReviewName>
+                <Stack spacing={1}>
+                  <Rating  name="size-small" defaultValue={review.reviewer_rating} precision={0.5} size="small"   readOnly />
+                </Stack>
+              </div>
+            </UserInfoReview>
             <ReviewComment>{review.comment}</ReviewComment>
-          </ReviewList>
+          </li>
         )
       })}
-    </ul>
+    </ReviewsList>
   )
 }
