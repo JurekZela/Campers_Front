5e69c9cff1bae1e6794ad67514072bb1a5e8f058
Juri Zelabowski <juri.zelabowski@gmail.com>
2025-04-17 19:29:40 +0200

    add Inter family

--- src/GlobalStyled-styled.js
+++ src/GlobalStyled-styled.js
@@ -4,8 +4,7 @@ const GlobalStyled = createGlobalStyle`
 body {
   margin: 0;
     padding: 0;
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
-    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    font-family: -apple-system, BlinkMacSystemFont, 'Inter',
     sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -26,7 +25,7 @@ h6,
 p, a {
   margin: 0;
     font-style: inherit;
-    font-family: Inter;
+    font-family: 'Inter', sans-serif;
 }
 ul,
 ol {
