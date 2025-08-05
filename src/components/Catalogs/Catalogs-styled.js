0da38228f4b01ca412281389d5a910d27680f739
Jurek Zelabowski <122160360+JurekZela@users.noreply.github.com>
2025-08-05 20:44:30 +0200

    start project at laptop

--- src/components/Catalogs/Catalogs-styled.js
+++ src/components/Catalogs/Catalogs-styled.js
@@ -1,5 +1,4 @@
 import { styled } from 'styled-components';
 
 export const Container = styled.div`
-    margin: 48px 64px 50px 64px;
 `;
--- src/components/Truck/TruckInfo-styled.js
+++ src/components/Truck/TruckInfo-styled.js
@@ -3,6 +3,7 @@ import { NavLink } from 'react-router-dom';
 
 export const CampersContainerPage = styled.div`
   display: flex;
+    padding: 48px 63px;
 `;
 
 export const TruckContainer = styled.ul`
@@ -16,7 +17,7 @@ export const TruckCard = styled.li`
     padding: 23px 24px 23px 23px;
     border: 1px solid #DADDE1;
     border-radius: 20px;
-    width: 888px;
+    max-width: 888px;
     min-height: 368px;
     height: auto;
     
